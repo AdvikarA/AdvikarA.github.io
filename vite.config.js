cb12e2369085aa3adfeafbd0bb7ae03dd0d34e33
Advikar Ananthkumar <advikar_ananthkumar@college.harvard.edu>
2025-06-10 23:40:38 -0400

    Update paths for GitHub Pages deployment and add GitHub Actions workflow

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+export default {
+  root: 'new-portfolio',
+  base: '/AdvikarA.github.io/',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  server: {
+    open: true
+  },
+  publicDir: 'assets'
+}
